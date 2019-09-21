e5c4cfa7770dfaa83e8df5196b28173c63bae40b
Brad Bow <brad.bow@stacktracehq.com>
2019-09-22 07:27:36 +1000

    new deploy script for new bucket

--- frontend/bin/deploy.sh
+++ frontend/bin/deploy.sh
@@ -4,4 +4,4 @@ echo "--------------- building static site..."
 npm run build
 
 echo "--------------- deploying to s3 bucket..."
-aws s3 sync build/ s3://omed-assist-website
+aws s3 sync build/ s3://www.omedassist.com
