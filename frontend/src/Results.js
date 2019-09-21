a2242bb02c0b9de04e45a52f3d6d78658b302c11
Ben Tefay <ben.tefay@stacktracehq.com>
2019-09-21 14:46:55 +1000

    Set up skeleton of app

---
+++ frontend/src/Results.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import styles from "./Results.module.css";
+
+class Results extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        Results
+      </div>
+    );
+  }
+}
+
+export default Results;
---
+++ frontend/src/Survey.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import styles from "./Survey.module.css";
+
+class Survey extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        Survey
+      </div>
+    );
+  }
+}
+
+export default Survey;
