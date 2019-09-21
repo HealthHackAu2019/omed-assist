52118af330b922d2656748451e80ef62e20172bd
Ben Tefay <ben.tefay@stacktracehq.com>
2019-09-21 17:47:08 +1000

    Restyle 404

--- frontend/src/NotFound/NotFound.js
+++ frontend/src/NotFound/NotFound.js
@@ -6,15 +6,17 @@ import image from "./404.svg";
 class NotFound extends Component {
   render() {
     return (
-      <div className={styles.container}>
-
-        <div className={styles.content}>
-          It looks like we're lost. Let's go back <Link to="/">home</Link>.
+      <>
+        <div className={styles.container}>
+          <img className={styles.image} src={image} />
         </div>
 
-        <img className={styles.image} src={image} />
-
-      </div>
+        <div className={styles.container}>
+          <div className={styles.content}>
+            It looks like we're lost. Let's go back <Link to="/">home</Link>.
+          </div>
+        </div>
+      </>
     );
   }
 }
