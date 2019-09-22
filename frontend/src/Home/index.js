9a5999fa2626feb6ef4b54b52b0e44a5824c13a6
BlueCodeThree <carliemhamilton@gmail.com>
2019-09-22 12:41:08 +1000

    adds styling to homepage with updated text

--- frontend/src/Home/index.js
+++ frontend/src/Home/index.js
@@ -19,6 +19,10 @@ const Home = () => {
                 <span className={styles.navigatePainText}>I want to navigate my pain relief journey</span>
                 <Link to="/questionnaire" className='btn btn-primary btn-lg'>Start Now</Link>
                 <span className={styles.painQuizText}>or take our <Link to="/quiz">Opioid Pain Quiz</Link></span>
+
+                <span className={styles.aboutText}>
+                    <p><strong>OMED Assist</strong> helps people who are being considered for opioid therapy navigate their pain relief journey. </p> <p>If you have experienced a recent injury or had surgery, OMED Assist can provide you with Opioid MEDication Assistance to guide your recovery.</p>
+                </span>
             </section>
         </div>
     )
